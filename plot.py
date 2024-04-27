cabe8f9c48539fc4a948f7655dcd108629a7c1c3
fura2 <yamazoe0577@gmail.com>
2024-04-28 01:39:37 +0900

    add RM

---
+++ plot.py
@@ -0,0 +1,51 @@
+import argparse
+import csv
+from pathlib import Path
+from typing import Optional
+
+import matplotlib.pylab as plt
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('csv_paths', nargs='+', help='CSV paths')
+    parser.add_argument('-a', '--alpha', type=float,
+                        default=1.0, help='Alpha value')
+    parser.add_argument('-o', '--output-path', type=Path,
+                        help='Output image path')
+    parser.add_argument('-d', '--dpi', type=float, default=200.0,
+                        help='Output image DPI (Default: 200)')
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = parse_args()
+    csv_paths: list = args.csv_paths
+    alpha: float = args.alpha
+    output_path: Optional[Path] = args.output_path
+    dpi: float = args.dpi
+
+    for i, path in enumerate(csv_paths):
+        path = Path(path)
+        with path.open('r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            rows = list(reader)
+        xs = [int(row['step']) for row in rows]
+        ys = [float(row['error']) for row in rows]
+        plt.plot(xs, ys, linewidth=0.5, alpha=alpha, label=f'sample {i+1}')
+
+    plt.xlabel('Step')
+    plt.ylabel('Error')
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.legend()
+
+    if output_path is None:
+        plt.show()
+    else:
+        output_path.parent.mkdir(parents=True, exist_ok=True)
+        plt.savefig(output_path, dpi=dpi)
+
+
+if __name__ == '__main__':
+    main()
---
+++ robbins-monro/run.py
@@ -0,0 +1,79 @@
+import argparse
+import csv
+from pathlib import Path
+
+import numpy as np
+
+
+################# Manual setup ##################
+
+target_value = 0.0
+true_solution = 0.0
+
+
+def true_func(x: float) -> float:
+    return x + 2.0 * np.sin(x)
+
+
+def noise(x: float) -> float:
+    sigma = 1.0
+    return np.random.normal(loc=0.0, scale=sigma)
+
+#################################################
+
+
+def observe(x: float) -> float:
+    return true_func(x) + noise(x)
+
+
+def robbins_monro(n_steps: int, step_coef: float, step_power: float) -> list[float]:
+    """
+    Compute a sample path until `n_steps` steps by the Robbins-Monro algorithm.
+    The n-th step size a_n is defined by a_n = c / n^e. where c = `step_coef` and e = `step_power`.
+    """
+    x = np.random.uniform(low=-10.0, high=10.0)
+    path = [x]
+    for i in range(n_steps):
+        y = observe(x)
+        a = step_coef / (i + 1)**step_power
+        x += a * (target_value - y)
+        path.append(x)
+    return path
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('n_steps', type=int, help='Number of steps')
+    parser.add_argument('output_path', type=Path, help='Output CSV path')
+    parser.add_argument('-c', '--step-coef', type=float, default=1.0,
+                        help='Coefficient in the step size (Default: 1.0)')
+    parser.add_argument('-p', '--step-power', type=float, default=1.0,
+                        help='Exponent in the step size (Default: 1.0)')
+    parser.add_argument('-s', '--seed', type=int, default=42,
+                        help='Seed of RNG (Default: 42)')
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = parse_args()
+    n_steps: int = args.n_steps
+    output_path: Path = args.output_path
+    step_coef: float = args.step_coef
+    step_power: float = args.step_power
+    seed: int = args.seed
+    assert n_steps >= 0, f'n_steps {n_steps} must be non-negative'
+
+    np.random.seed(seed)
+
+    result = robbins_monro(n_steps, step_coef, step_power)
+
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+    with output_path.open('w', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=['step', 'value', 'error'])
+        writer.writeheader()
+        writer.writerows([{'step': i, 'value': x, 'error': np.abs(x - true_solution)}
+                          for i, x in enumerate(result)])
+
+
+if __name__ == '__main__':
+    main()
